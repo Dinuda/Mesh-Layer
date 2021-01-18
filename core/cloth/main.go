61fe037a179d6202b04ba79e04b4009576110e89
Tarith <tarithj@gmail.com>
2021-01-18 15:02:45 +0530

    ref: made the fabric.Handle func return a http.HandleFunc

--- core/cloth/main.go
+++ core/cloth/main.go
@@ -9,24 +9,27 @@ import (
 
 type Cloth []fabric.Fabric
 
-func (c *Cloth) Handle(w http.ResponseWriter, r *http.Request) {
-	handled := false
-	killed := false
-	for i := 0; i < len(*c); i++ {
-		uC := *c
-		if html.EscapeString(r.URL.Path) == uC[i].Path {
-			s := fabric.Strand{Parent: &uC[i], Rw: &w, R: r, Killed: &killed}
-			for i := uint(0); i < s.GetMeshCount(); i++ {
-				s.NextMesh()
+// returns a http.HandlerFunc
+func (c *Cloth) Handle() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handled := false
+		killed := false
+		for i := 0; i < len(*c); i++ {
+			uC := *c
+			if html.EscapeString(r.URL.Path) == uC[i].Path {
+				s := fabric.Strand{Parent: &uC[i], Rw: &w, R: r, Killed: &killed}
+				for i := uint(0); i < s.GetMeshCount(); i++ {
+					s.NextMesh()
+				}
+				if !*s.Killed {
+					s.E()
+				}
+				handled = true
+				break
 			}
-			if !*s.Killed {
-				s.E()
-			}
-			handled = true
-			break
 		}
-	}
-	if !handled {
-		_, _ = fmt.Fprintln(w, []byte("Could not find path"))
+		if !handled {
+			_, _ = fmt.Fprintln(w, []byte("Could not find path"))
+		}
 	}
 }
